3431289e0eaae04b27d5882920c2f20d974d122a
aporo4000 <hiroshi4000@gmail.com>
2018-12-31 04:32:15 +0900

    vue.js 算出プロパティ&メソッド

--- helloVueJS/js/main.js
+++ helloVueJS/js/main.js
@@ -1,6 +1,59 @@
-new Vue({
-    template: '<p>{{ msg }}</p>',
-    data: {
-        msg: 'hello Vue.js'
+var items = [
+    {
+        name: '鉛筆',
+        price: 300,
+        quantity: 0
+    },
+    {
+        name: 'ノート',
+        price: 400,
+        quantity: 0
+    },
+    {
+        name: '消しゴム',
+        price: 500,
+        quantity: 0
     }
-}).$mount('#app')
+]
+var vm = new Vue({
+    el: '#app',
+    data: {
+        items: items
+    },
+    filters: {
+        numberWithDelimiter: function(value){
+            if(!value){
+                return '0'
+            }
+            return value.toString().replace(/(¥d)(?=(¥d{3})+$)/g, '$1,')
+        }
+    },
+    methods: {
+        doBuy: function () {
+            alert(this.totalPriceWithTax + '円のお買い上げ!')
+            this.items.forEach(function (item){
+                item.quantity = 0
+            })
+        }
+    },
+    computed: {
+        totalPrice: function () {
+            return this.items.reduce(function (sum, item){
+                return sum + (item.price * item.quantity)
+            }, 0)
+        },
+        totalPriceWithTax: function () {
+            return Math.floor(this.totalPrice * 1.08)
+        },
+        canBuy: function () {
+            return this.totalPrice >= 1000  // 1000円以上から購入可能
+        },
+        errorMessageStyle: function(){
+            return {
+                border: this.canBuy ? '' : '1px solid red',
+                color: this.canBuy ? '' : 'red'
+            }
+        }
+    }
+});
+window.vm = vm
